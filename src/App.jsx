93363651a1c2af755c7d75305ae525fe14d6b101
mrBidone <ab.bz@ukr.net>
2025-01-21 17:08:53 +0100

    Find and fixed problem with error-undefined

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "./redux/contactsOps";
-import { selectLoading } from "./redux/contactsSlice";
+import { selectError, selectLoading } from "./redux/contactsSlice";
 import Loader from "./components/Loader/Loader";
 
 function App() {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -21,6 +22,11 @@ function App() {
       <ContactForm />
       <SearchBox />
       {!isLoading ? <ContactList /> : <Loader />}
+      {isError && (
+        <p style={{ color: "red" }}>
+          OOOPS... Error: "{isError}". Please try again later.
+        </p>
+      )}
     </>
   );
 }
--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,10 +3,10 @@ import axios from "axios";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
-  async (thunkApi) => {
+  async (_, thunkApi) => {
     try {
       const { data } = await axios.get(
-        "https://678aa23cdd587da7ac2aeb83.mockapi.io/api/v1/contacts"
+        "https://678aa23cdd587da7ac2aeb83.moc22kapi.io/api/v1/contacts"
       );
       return data;
     } catch (error) {
